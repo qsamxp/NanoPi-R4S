ab70364c4dd33cec060aa3f9de8ed445dc130c86
LewiVir <38184679+LewiVir@users.noreply.github.com>
2020-12-12 11:57:35 +0800

    20201212 fix r4s patch

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+cp patches/200-rockchip-add-support-for-NanoPi-R4S.patch.new openwrt/target/linux/rockchip/patches-5.4/200-rockchip-add-support-for-NanoPi-R4S.patch
